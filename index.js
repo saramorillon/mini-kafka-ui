c3401910ea2cfd3a701eee8fd02a94a1872799b1
Sara Morillon <sara.morillon-ext@kering.com>
2021-08-19 12:20:23 +0000

    Package app

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
     webPreferences: { nodeIntegration: true, contextIsolation: false },
-  });
-  mainWindow.setMenu(null);
-  mainWindow.loadURL("http://localhost:4000");
-  mainWindow.webContents.openDevTools({ mode: "undocked", activate: false });
+  })
+  mainWindow.setMenu(null)
+  if (process.env.NODE_ENV === 'dev') {
+    mainWindow.loadURL('http://localhost:4000')
+    mainWindow.webContents.openDevTools({ mode: 'undocked', activate: false })
+  } else {
+    mainWindow.loadFile(path.join(process.cwd(), 'resources', 'app', 'index.html'))
+  }
 }
 
-app.on("ready", () => {
-  createWindow();
+app.on('ready', () => {
+  createWindow()
 
-  app.on("activate", function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
-});
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
