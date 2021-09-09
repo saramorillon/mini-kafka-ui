248ed38e823c7aef5db6686b0438bc2de69f6f27
Sara Morillon <sara.morillon-ext@kering.com>
2021-09-09 13:00:53 +0000

    Add ability to close connection

--- src/ui/Connections/Connections.tsx
+++ src/ui/Connections/Connections.tsx
@@ -1,30 +1,46 @@
-import { Pivot, PivotItem } from '@fluentui/react'
-import React, { useCallback, useContext } from 'react'
+import { IconButton, Pivot, PivotItem } from '@fluentui/react'
+import React, { useContext } from 'react'
 import { ConnectionsContext } from '../../contexts/ConnectionsContext'
+import { IConnection } from '../../models/IConnection'
 import { Messages } from '../Messages/Messages'
 
 export function Connections(): JSX.Element {
-  const { activeConnection, openConnections, dispatch } = useContext(ConnectionsContext)
-
-  const onLinkClick = useCallback(
-    (item?: PivotItem) => {
-      if (item?.props.itemKey) dispatch({ type: 'open', key: item.props.itemKey })
-    },
-    [dispatch]
-  )
+  const { activeConnection, openConnections } = useContext(ConnectionsContext)
 
   return (
-    <Pivot
-      overflowBehavior="menu"
-      style={{ overflow: 'hidden', flex: 1 }}
-      selectedKey={activeConnection}
-      onLinkClick={onLinkClick}
-    >
+    <Pivot overflowBehavior="menu" style={{ overflow: 'hidden', flex: 1 }} selectedKey={activeConnection}>
       {openConnections.map((connection) => (
-        <PivotItem key={connection.key} headerText={connection.name} itemKey={connection.key}>
+        <PivotItem
+          key={connection.key}
+          itemKey={connection.key}
+          onRenderItemLink={() => <CustomPivotItem connection={connection} />}
+          alwaysRender
+        >
           <Messages connection={connection} />
         </PivotItem>
       ))}
     </Pivot>
+  )
+}
+
+interface ICustomPivotItemProps {
+  connection: IConnection
+}
+
+function CustomPivotItem({ connection }: ICustomPivotItemProps) {
+  const { dispatch } = useContext(ConnectionsContext)
+
+  const { key, name } = connection
+
+  return (
+    <>
+      <span onClick={() => dispatch({ type: 'open', key })}>{name}</span>
+      <IconButton
+        iconProps={{ iconName: 'ChromeClose' }}
+        title="Close"
+        styles={{ icon: { fontSize: '0.5rem' } }}
+        onClick={() => dispatch({ type: 'close', key })}
+      />
+    </>
   )
 }
