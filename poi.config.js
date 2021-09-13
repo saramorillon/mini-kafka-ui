2d99fd9cbba837966443e74d4db40a8b5266c711
Your Name <you@example.com>
2021-09-13 18:16:35 +0200

    Add packaging for linux

--- poi.config.js
+++ poi.config.js
@@ -1,5 +1,10 @@
+const CopyPlugin = require('copy-webpack-plugin')
+
 module.exports = {
   entry: 'src/index.tsx',
   output: { dir: 'dist', publicUrl: '.', target: 'electron-renderer' },
   plugins: [{ resolve: '@poi/plugin-typescript' }],
+  configureWebpack: {
+    plugins: [new CopyPlugin([{ from: 'package.json' }, { from: 'index.js' }])],
+  },
 }
