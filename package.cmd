950525340d32a06aedac0ea5d6b4a5c51dda6a9e
Sara Morillon <sara.morillon.pro@gmail.com>
2022-06-23 16:02:03 +0200

    Fix package command

--- package.cmd
+++ package.cmd
@@ -1,8 +1,8 @@
 @echo off
 
-del back\mz-utils-win32-x64.zip /q
+del back\mini-kafka-ui-win32-x64.zip /q
 rmdir /s back\dist /q
-rmdir /s back\mz-utils-win32-x64 /q
+rmdir /s back\mini-kafka-ui-win32-x64 /q
 rmdir /s front\dist /q
 
 call yarn --cwd back build
@@ -12,6 +12,6 @@ xcopy back\node_modules\ back\dist\node_modules\ /q /s
 call yarn --cwd back package
 
 call yarn --cwd front build
-xcopy front\dist\ back\mz-utils-win32-x64\resources\app\public\ /q /s
+xcopy front\dist\ back\mini-kafka-ui-win32-x64\resources\app\public\ /q /s
 
 call yarn --cwd back zip
