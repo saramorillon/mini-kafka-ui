6e2093e7d0e99b0a6c5b94bd681320c7bfa94007
Sara Morillon <sara.morillon.pro@gmail.com>
2022-07-21 16:12:32 +0200

    Change Favorite model

--- back/src/main/java/com/saramorillon/models/Server.java
+++ back/src/main/java/com/saramorillon/models/Server.java
@@ -4,4 +4,9 @@ public class Server {
     public String key;
     public String name;
     public String brokers;
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj != null && (obj instanceof Server) && ((Server) obj).key == this.key;
+    }
 }
